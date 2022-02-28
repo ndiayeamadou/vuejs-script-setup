0630d644a1180a3c562364ec058288bdc4e86cba
Amadu <elhadjiamadoundiaye@gmail.com>
2022-02-28 12:06:11 +0000

    setup function with vue (old)

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Testing...</h2>
+  <h3 @click="handleHeadingClick">Abbreviation name : {{name}} | age : {{age}}</h3>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const name = ref('AMADU')
+    const age = ref(32)
+
+    const handleHeadingClick = () => {
+      name.value = "ELAN"
+      age.value = 45
+    }
+
+    return {
+      age, name, handleHeadingClick
+    }
+    
+  },
+}
+</script>
